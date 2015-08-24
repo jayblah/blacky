97da9e3659df5b68a95934bd2191d50166461364
DZ191 <darkprince97@live.it>
2015-08-24 09:26:09 +0200

    Lissandra: 2nd Fix

--- Lissandra the Ice Goddess/Lissandra.cs
+++ Lissandra the Ice Goddess/Lissandra.cs
@@ -328,7 +328,7 @@ namespace Lissandra_the_Ice_Goddess
                     }
                     else
                     {
-                        if (CurrentEPosition.Distance(comboTarget.ServerPosition) <= 200f || CurrentEPosition.Distance(EEnd) <= 100f)
+                        if (CurrentEPosition.Distance(comboTarget.ServerPosition) <= SkillsHandler.Spells[SpellSlot.W].Range/2f || CurrentEPosition.Distance(EEnd) <= 100f)
                         {
                             if (CurrentEPosition.IsSafePositionEx() && CurrentEPosition.PassesNoEIntoEnemiesCheck())
                             {
